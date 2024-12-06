0c0e9a41599a6c8f4fe0c85ef10ba5ce30b61bb2
Daniel Khen <danielkhen20@gmail.com>
2024-12-06 23:49:27 +0200

    test

--- modules.py
+++ modules.py
@@ -1,5 +1,7 @@
 from torch.nn import Module, Conv2d, GroupNorm, LayerNorm, BatchNorm2d
 import torch.nn.utils.parametrize as parametrize
+from opacus.grad_sample import register_grad_sampler
+
 
 NORM_LAYERS = (GroupNorm, LayerNorm, BatchNorm2d)
 
@@ -10,11 +12,16 @@ class Standardization(Module):
         std = X.view(X.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
 
         return (X - mean) / std.expand_as(X)
-    
+
+# This module doesn't have parameters
+@register_grad_sampler(Standardization)
+def compute_standardization_grad_sample(layer, activations, backprops):
+    return {}
+
 def standardize_model(model):
     modules = list(model.modules())
     
     # Register weight standardization parameterization if layer is Conv2d and next layer is a normalization layer
     for module, next_module in zip(modules[:-1], modules[1:]):
         if isinstance(module, Conv2d) and isinstance(next_module, NORM_LAYERS):
-            parametrize.register_parametrization(module, "weight", Standardization())
+            parametrize.register_parametrization(module, 'weight', Standardization())
