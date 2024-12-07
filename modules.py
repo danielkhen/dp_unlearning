f6a23ed29b1b30879c2f3504691cd2890e27ca8c
Daniel Khen <danielkhen20@gmail.com>
2024-12-07 03:06:52 +0200

    use conv2dws

--- modules.py
+++ modules.py
@@ -1,7 +1,3 @@
-from torch.nn import Module, Conv2d, GroupNorm, LayerNorm, BatchNorm2d
-import torch.nn.utils.parametrize as parametrize
-from opacus.grad_sample import register_grad_sampler
-
 from torch.nn import Conv2d, GroupNorm, LayerNorm, BatchNorm2d, functional
 
 NORM_LAYERS = (GroupNorm, LayerNorm, BatchNorm2d)
@@ -29,15 +25,15 @@ class Conv2dWS(Conv2d):
 
     def forward(self, x):
         weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                  keepdim=True).mean(dim=3, keepdim=True)
+        weight_mean = weight.mean(dim=[1, 2, 3], keepdim=True)
         weight = weight - weight_mean
         std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
         weight = weight / std.expand_as(weight)
 
         return functional.conv2d(x, weight, self.bias, self.stride,
                                 self.padding, self.dilation, self.groups)
-    
+
+# Implementation with parameterization didn't work with grad_sample_mode=hooks and didn't have performance benefits
 def standardize_model(model):
     children = list(model.named_children())
 
@@ -48,22 +44,3 @@ def standardize_model(model):
             setattr(model, name, Conv2dWS(child))
         else:
             standardize_model(child) # Continue recursivly for child modules
-
-
-NORM_LAYERS = (GroupNorm, LayerNorm, BatchNorm2d)
-
-# # Weight standardization parameterization module
-# class Standardization(Module):
-#     def forward(self, X):
-#         mean = X.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
-#         std = X.view(X.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
-
-#         return (X - mean) / std.expand_as(X)
-
-# def standardize_model(model):
-#     modules = list(model.modules())
-    
-#     # Register weight standardization parameterization if layer is Conv2d and next layer is a normalization layer
-#     for module, next_module in zip(modules[:-1], modules[1:]):
-#         if isinstance(module, Conv2d) and isinstance(next_module, NORM_LAYERS):
-#             parametrize.register_parametrization(module, 'weight', Standardization())
