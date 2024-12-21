0c33086960170b40bd22665b7f4b4af6f36797f3
Daniel Khen <danielkhen20@gmail.com>
2024-12-21 18:52:32 +0200

    try polynomial

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def main():
     schedulers = []
 
     if args.cosine_anealing:
-        schedulers.append(optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs))
+        schedulers.append(optim.lr_scheduler.PolynomialLR(optimizer, total_iters=args.epochs, power=2.0, verbose=True))
 
     if args.exponential_moving_average:
         ema_model = AveragedModel(model, multi_avg_fn=get_ema_multi_avg_fn(args.ema_decay), use_buffers=True, device=static.CUDA)
