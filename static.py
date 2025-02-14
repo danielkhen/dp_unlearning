05eed80e4f7405e3139a0a0a5f44c4decbd68658
Daniel Khen <danielkhen20@gmail.com>
2025-02-14 14:22:11 +0200

    a

--- static.py
+++ static.py
@@ -19,7 +19,7 @@ DATASET_MEAN = (0.4914, 0.4822, 0.4465)
 DATASET_STD = (0.2023, 0.1994, 0.2010)
 
 AUGMENTATIONS = (
-        #transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+        transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
 )
